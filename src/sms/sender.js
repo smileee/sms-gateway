d2e44ed50de1003ca7409e30e65b60f76cbfe2a2
Guilherme Froes <94195233+smileee@users.noreply.github.com>
2025-05-08 11:53:41 -0400

    Update SMSSender to configure modem settings for new version compatibility by adjusting CSMP parameters for both text and UCS2 message types, enhancing SMS transmission reliability.

--- src/sms/sender.js
+++ src/sms/sender.js
@@ -25,11 +25,13 @@ class SMSSender {
     // Configure modem
     await this.atManager.send('AT+CMEE=2'); // Enable error messages
     await this.atManager.send('AT+CMGF=1'); // Set message format to text
-    await this.atManager.send('AT+CSMP=17,167,0,0'); // Set message type to text
+    // await this.atManager.send('AT+CSMP=17,167,0,0'); // Set message type to text
+    await this.atManager.send('AT+CSMP=17,23,64,244'); // The difference between the two is that the second one is for the new version of the modem
     
     if (useUCS2) {
       await this.atManager.send('AT+CSCS="UCS2"'); // Set character set to UCS2
-      await this.atManager.send('AT+CSMP=17,167,0,8'); // Set message type to UCS2
+      // await this.atManager.send('AT+CSMP=17,167,0,8'); // Set message type to UCS2
+      await this.atManager.send('AT+CSMP=17,23,64,244'); // The difference between the two is that the second one is for the new version of the modem
     }
     
     // Send SMS (expecting prompt '>')
